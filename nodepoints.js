95f0dc5ee4c1d4241c9f868454119ee9ef2add82
Harsha-Kolekar <167529898+Harsha-Kolekar@users.noreply.github.com>
2025-02-01 13:02:18 +0530

    files are added

---
+++ nodepoints.js
@@ -0,0 +1,29 @@
+// console.log("hii");
+// const { createServer } = require('node:http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+// const fs=require("fs");
+// const http=require('http')
+// const fs=require('fs')
+// const fileContent=fs.readFileSync('index.html')
+// const server=http.createServer((req,res)=>
+// {
+//   res.writeHead(200,{'Content-type':'text/html'});
+//   res.end(fileContent)
+
+// })
+// server.listen(80,'127.0.0.1',()=>{
+//   console.log("listening on port 80")
+// })
+
